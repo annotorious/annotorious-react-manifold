8a9688fc9b2fce443389b775e1d99934e003a8ba
Rainer Simon <rainer@rainersimon.io>
2023-11-08 14:53:18 +0100

    Tweaked API + added essential missing methods

--- src/AnnotoriousManifoldInstance.ts
+++ src/AnnotoriousManifoldInstance.ts
@@ -1,8 +1,10 @@
 import { 
   Annotation, 
+  AnnotationBody, 
   Annotator, 
   DrawingStyle, 
-  LifecycleEvents 
+  LifecycleEvents, 
+  Origin
 } from '@annotorious/core';
 
 export interface AnnotoriousManifoldInstance<I extends Annotation = Annotation, E extends unknown = Annotation> {
@@ -11,19 +13,17 @@ export interface AnnotoriousManifoldInstance<I extends Annotation = Annotation, 
 
   style: DrawingStyle | ((annotation: I) => DrawingStyle) | undefined;
 
-  clearAnnotations(): void;
+  addBody(body: AnnotationBody, origin: Origin): void;
+
+  clear(origin: Origin): void;
+
+  deleteAnnotation(id: string, origin: Origin): E | undefined;
 
   destroy(): void;
 
   getAnnotationById(id: string): E | undefined;
 
   getAnnotations(): E[];
-
-  removeAnnotation(arg: E | string): E;
-
-  setAnnotations(annotations: E[]): void;
-
-  updateAnnotation(annotation: E): E;
   
   on<T extends keyof LifecycleEvents<E>>(event: T, callback: LifecycleEvents<E>[T]): void;
 
@@ -35,22 +35,57 @@ export const createManifoldInstance = <I extends Annotation = Annotation, E exte
   annotators: Map<string, Annotator<I, E>>
 ): AnnotoriousManifoldInstance<I, E> => {
 
-  const clearAnnotations = () =>
-    Array.from(annotators.values()).forEach(a => a.clearAnnotations());
+  const find = (annotationId: string): { annotation?: E, annotator?: Annotator<I, E> } =>
+    Array.from(annotators.values()).reduce((found, annotator) => {
+      if (found)
+        return found;
+
+      const annotation = annotator.getAnnotationById(annotationId);
+      if (annotation) 
+        return { annotation, annotator };
+    }, undefined as { annotation: E, annotator: Annotator<I, E> } | undefined ) || 
+
+    { annotation: undefined, annotator: undefined };
+
+  /** API **/
+
+  const addBody = (body: AnnotationBody, origin = Origin.LOCAL) => {
+    const { annotator } = find(body.annotation);
+    if (annotator)
+      annotator.state.store.addBody(body, origin);
+  }
+
+  const clear = (origin = Origin.LOCAL) =>
+    Array.from(annotators.values()).forEach(a => a.state.store.clear(origin));
+
+  const deleteAnnotation = (id: string, origin = Origin.LOCAL) => {
+    const { annotation, annotator } = find(id);
+
+    if (annotator) {
+      annotator.state.store.deleteAnnotation(id, origin);
+      return annotation;
+    }
+  }
 
   const destroy = () =>
     Array.from(annotators.values()).forEach(a => a.destroy());
 
-  const getAnnotationById = (id: string) =>
-    Array.from(annotators.values()).reduce((found, annotator) => 
-      found ? found : annotator.getAnnotationById(id), undefined as E | undefined);
+  const getAnnotationById = (id: string) => 
+    find(id).annotation;
+
+  const getAnnotations = () => 
+    Array.from(annotators.values()).reduce((all, annotator) =>
+      [...all, ...annotator.getAnnotations()], [] as E[])
 
   // @ts-ignore
   return {
     annotators: [...annotators.values()],
-    clearAnnotations,
+    addBody,
+    clear,
+    deleteAnnotation,
     destroy,
-    getAnnotationById
+    getAnnotationById,
+    getAnnotations
   }
 
 }
